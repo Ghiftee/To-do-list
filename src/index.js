9e6921e6263a6e1a428d8fe35862c0d93e887590
Ghiftee <gigibetine@gmail.com>
2021-07-12 14:22:15 +0100

    Add styling

--- src/index.js
+++ src/index.js
@@ -19,18 +19,20 @@ const tasks = [
 ];
 
 function populate() {
-    // Got element by id from html
     const taskList = document.getElementById('task-list');
-    // Cleared anything in the taskList previously
     taskList.innerHTML = '';
-    // Loop over tasks array but first sort according to index
     const sortedTask = tasks.sort((a,b) => a.index - b.index);
     sortedTask.forEach((task) => {
-        // Create a new list item
         const listItem = document.createElement('li');
-        // set list item text to description
-        listItem.innerHTML = task.description
-        // add li element to ul in html with id of task-list
+        listItem.classList.add("border-bottom", "p-3", "pt-2", "pb-2", "d-flex", "flex-row", "justify-content-between", "align-items-center");
+        const spanItem = document.createElement('span');
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.classList.add('me-2');
+        const menu = document.createElement('i');
+        menu.classList.add('fa', 'fa-ellipsis-v', 'text-secondary');
+        spanItem.append(checkBox, task.description);
+        listItem.append(spanItem, menu);
         taskList.append(listItem);
     })
 }

9418f77211f2bbcbd243bc1aa412bf30beac630d
Ghiftee <gigibetine@gmail.com>
2021-07-13 15:59:20 +0100

    Fix local storage bug

--- src/status.js
+++ src/status.js
@@ -3,6 +3,7 @@ function updateStatus(textElement, checkBox, task) {
   const tick = document.createElement('i');
   tick.classList.add('fa', 'fa-check', 'text-primary', 'me-1');
   checkBox.parentNode.replaceChild(tick, checkBox);
+  task.completed = true;
 
   const allTasks = JSON.parse(localStorage.getItem('tasks'));
   const currentTask = allTasks.find((t) => t.index === task.index);

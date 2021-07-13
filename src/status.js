a3265333d4c58d7866c4a10a717cd60f3cc1b64e
Ghiftee <gigibetine@gmail.com>
2021-07-13 17:25:07 +0100

    Create function to uncomplete task

--- src/status.js
+++ src/status.js
@@ -1,13 +1,15 @@
 function updateStatus(textElement, checkBox, task) {
-  textElement.classList.add('strike', 'text-muted');
-  const tick = document.createElement('i');
-  tick.classList.add('fa', 'fa-check', 'text-primary', 'me-1');
-  checkBox.parentNode.replaceChild(tick, checkBox);
-  task.completed = true;
-
   const allTasks = JSON.parse(localStorage.getItem('tasks'));
   const currentTask = allTasks.find((t) => t.index === task.index);
-  currentTask.completed = true;
+  textElement.classList.toggle('strike');
+  textElement.classList.toggle('text-muted');
+  if (checkBox.checked) {
+    task.completed = true;
+    currentTask.completed = true;
+  } else {
+    task.completed = false;
+    currentTask.completed = false;
+  }
   localStorage.setItem('tasks', JSON.stringify(allTasks));
 }
 

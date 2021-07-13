d7a37cd5afc61b83ab8f28f3ae96e772bb9450ae
Ghiftee <gigibetine@gmail.com>
2021-07-13 15:06:40 +0100

    Correct style for dragging

--- src/drag.js
+++ src/drag.js
@@ -3,7 +3,7 @@ let currentlyDraggging = 0;
 
 function dragStart(e) {
   currentlyDraggging = e.target.id;
-  e.target.ClassList.add('border', 'border-primary');
+  e.target.classList.add('border', 'border-primary');
 }
 
 function dragOver(e) {

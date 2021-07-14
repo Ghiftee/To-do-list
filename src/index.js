025056dc1735e5e7519953aa507e0792d04cbdc1
Ghiftee <gigibetine@gmail.com>
2021-07-14 06:43:07 +0100

    Create function for adding new tasks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+
 import { dragStart, dragOver, drop } from './drag.js';
 import {
   addNewTask, editTask, deleteTask, clearAllCompleted,
@@ -8,7 +9,6 @@ import updateStatus from './status.js';
 const taskList = document.getElementById('task-list');
 
 let allTasks = [
-  
 ];
 
 function populate(tasks) {
@@ -17,40 +17,40 @@ function populate(tasks) {
   tasks.forEach((task) => {
     const listItem = document.createElement('li');
     listItem.draggable = true;
-    listItem.classList.add(
-      'border-bottom',
-      'p-3',
-      'pt-2',
-      'pb-2',
-      'd-flex',
-      'flex-row',
-      'justify-content-between',
-      'align-items-center',
-    );
+    listItem.classList.add('border-bottom', 'p-3', 'pt-2', 'pb-2', 'd-flex', 'flex-row', 'justify-content-between', 'align-items-center');
     listItem.id = task.index;
 
     const spanItem = document.createElement('span');
     const textElement = document.createElement('p');
-
     textElement.innerText = task.description;
     textElement.classList.add('d-inline');
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('me-2');
     const menu = document.createElement('i');
-    menu.classList.add('fa', 'fa-ellipsis-v', 'text-secondary');
+    menu.classList.add('fa', 'fa-ellipsis-v', 'text-secondarys');
+    menu.addEventListener('click', (e) => {
+      editTask(e, listItem, textElement, spanItem, allTasks, task, populate);
+      menu.addEventListener('click', () => {
+        deleteTask(task);
+        allTasks = JSON.parse(localStorage.getItem('tasks'));
+        const sortedTasks = allTasks.sort((a, b) => a.index - b.index);
+        populate(sortedTasks);
+      });
+    });
+    spanItem.append(checkBox, textElement);
 
     listItem.append(spanItem, menu);
     taskList.append(listItem);
 
     listItem.addEventListener('dragover', dragOver);
     listItem.addEventListener('dragstart', dragStart);
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.classList.add('me-2');
 
     if (task.completed) {
       textElement.classList.add('strike', 'text-muted');
       checkBox.checked = true;
     }
-    spanItem.append(checkBox, textElement);
+
     checkBox.addEventListener('change', () => {
       updateStatus(textElement, checkBox, task);
     });
@@ -68,5 +68,9 @@ if (localStorage.getItem('tasks')) {
   localStorage.setItem('tasks', JSON.stringify(allTasks));
 }
 
-const sortedTasks = allTasks.sort((a, b) => a.index - b.index);
-document.addEventListener('DOMContentLoaded', populate(sortedTasks));
+document.getElementById('add-button').addEventListener('click', () => {
+  addNewTask();
+  allTasks = JSON.parse(localStorage.getItem('tasks'));
+  const sortedTasks = allTasks.sort((a, b) => a.index - b.index);
+  populate(sortedTasks);
+});
